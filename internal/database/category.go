20c45f39214c46c03ed206dbe49680782b3bacd6
bruno.gluszczuk <brunogluszczukjobs@gmail.com>
2025-05-20 19:40:29 -0300

    feat: find all categories

--- internal/database/category.go
+++ internal/database/category.go
@@ -5,7 +5,7 @@ import (
 	"github.com/google/uuid"
 )
 
-type Category struct {
+type  Category struct {
 	db          *sql.DB
 	ID          string
 	Name        string
@@ -28,4 +28,26 @@ func (c *Category) Create(name, description string) (Category, error) {
 		Name:        name,
 		Description: description,
 	}, nil
+}
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	categories := []Category{}
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{
+			ID:          id,
+			Name:        name,
+			Description: description,
+		})
+	}
+	return categories, nil
 }
