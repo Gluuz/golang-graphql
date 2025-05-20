b2f9411717c1da1ef54c279153782e72baa02d3a
bruno.gluszczuk <brunogluszczukjobs@gmail.com>
2025-05-20 19:21:55 -0300

    feat: persisting category

--- cmd/server.go
+++ cmd/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"golang-graphql/graph"
+	"golang-graphql/internal/database"
 	"log"
 	"net/http"
 	"os"
@@ -11,18 +13,28 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+	
+	categoryDB := database.NewCategory(db)
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+			CategoryDB: categoryDB,
+	}}))
 
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
